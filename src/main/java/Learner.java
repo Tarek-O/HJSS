7a2aea8a8f3229b9d417bd91557f87da4ac3c8fa
Tarek <tarekomran_98@hotmail.com>
2024-03-08 16:18:33 +0000

    Adding test class for practice

--- src/main/java/Learner.java
+++ src/main/java/Learner.java
@@ -1,16 +1,24 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.Date;
+
 public class Learner {
 
-    private String name, gender, emergencyContactNumber;
-    private int age, gradeLevel;
+    private String name, emergencyContactNumber;
+    private boolean gender;
+    private int gradeLevel;
+    private Date birthDate;
 
     //Format of the UK Phone Number using Regex. Accepts country code as well.
     private final String phoneNumberRegex = "^(((\\+44\\s?\\d{4}|\\(?0\\d{4}\\)?)\\s?\\d{3}\\s?\\d{3})|((\\+44\\s?\\d{3}|\\(?0\\d{3}\\)?)\\s?\\d{3}\\s?\\d{4})|((\\+44\\s?\\d{2}|\\(?0\\d{2}\\)?)\\s?\\d{4}\\s?\\d{4}))(\\s?\\#(\\d{4}|\\d{3}))?$";
 
     public Learner(){};
-    public Learner(String name, String gender, int age, String emergencyContactNumber, int gradeLevel) throws Exception {
+    public Learner(String name, boolean gender, Date birthDate, String emergencyContactNumber, int gradeLevel) throws Exception {
         setName(name);
         setGender(gender);
-        setAge(age);
+        setBirthDate(birthDate);
         setEmergencyContactNumber(emergencyContactNumber);
         setGradeLevel(gradeLevel);
     }
@@ -24,25 +32,19 @@ public class Learner {
         this.name = name;
     }
 
-    public String getGender() {
+    public boolean getGender() {
         return gender;
     }
 
-    public void setGender(String gender) {
+    public void setGender(boolean gender) {
         this.gender = gender;
     }
 
-    public int getAge() {
-        return age;
-    }
+    public Date getBirthDate() { return birthDate; }
 
-    public void setAge(int age) throws Exception {
+    public void setBirthDate(Date dateOfBirth) throws Exception{
         //Age should be between 4 to 11 requirement
-        if(age >= 4 && age <= 11){
-            this.age = age;
-        }else{
-            throw new Exception("The age should be between 4 to 11 years old only.");
-        }
+        this.birthDate = dateOfBirth;
     }
 
     public String getEmergencyContactNumber() {
@@ -66,4 +68,9 @@ public class Learner {
         this.gradeLevel = gradeLevel;
     }
 
+
+    public double calculateAge(){
+        Date dateNow = new Date();
+        return (double) ((dateNow.getTime() - getBirthDate().getTime()) / 31540000) / 1000;
+    }
 }
