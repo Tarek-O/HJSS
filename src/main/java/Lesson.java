6c309643acbd6a08e91c95b3e4e0bd16fca450bd
Tarek <tarekomran_98@hotmail.com>
2024-03-11 22:29:56 +0000

    Added generate ID using the object's fields and refactored constructors accordingly. Removed redundant methods of getting day and month.

---
+++ src/main/java/Lesson.java
@@ -0,0 +1,109 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class Lesson {
+
+    private String id;
+    private LocalDate dateOfLesson;
+    private int gradeLevel;
+    private ArrayList<Learner> listOfLearners;
+    private String timeSlot;
+    private final int maxNumberOfLearners = 4;
+
+    /**
+     * Constructor for Lesson where the ID is automatically generated based on input.
+     * @param dateOfLesson
+     * @param gradeLevel
+     * @param timeSlot
+     * @param listOfLearners
+     */
+    public Lesson(LocalDate dateOfLesson, int gradeLevel, String timeSlot, ArrayList<Learner> listOfLearners){
+        setDateOfLesson(dateOfLesson);
+        setGradeLevel(gradeLevel);
+        setTimeSlot(timeSlot);
+        this.listOfLearners = listOfLearners;
+        setId(generateLessonID());
+    }
+
+    /**
+     * Constructor for Lesson where the ID is automatically generated based on input.
+     * @param dateOfLesson
+     * @param gradeLevel
+     * @param timeSlot
+     */
+    public Lesson(LocalDate dateOfLesson, int gradeLevel, String timeSlot){
+        setDateOfLesson(dateOfLesson);
+        setGradeLevel(gradeLevel);
+        setTimeSlot(timeSlot);
+        listOfLearners = new ArrayList<Learner>();
+        setId(generateLessonID());
+    }
+
+    /**
+     *
+     * @return Return boolean if the list of Learners can be appended.
+     */
+    public boolean hasAvailableSpot(){
+        return listOfLearners.size() < maxNumberOfLearners;
+    }
+
+    /**
+     *
+     * @return Size of Learners
+     */
+    public int getNumberOfLearners(){
+        return listOfLearners.size();
+    }
+
+    /**
+     * The ID of the Lesson is generated using the Grade Level, Time Slot, Day of the Week, Date, Month and Year.
+     * @return ID of Lesson
+     */
+    public String generateLessonID(){
+        return getGradeLevel() + "GR" + getTimeSlot() + getDateOfLesson().getDayOfWeek().toString().substring(0,3).toUpperCase() + getDateOfLesson().getDayOfMonth() + getDateOfLesson().getMonth().toString().substring(0,3).toUpperCase()  + (getDateOfLesson().getYear()%100);
+    }
+
+    public LocalDate getDateOfLesson() {
+        return dateOfLesson;
+    }
+
+    public void setDateOfLesson(LocalDate dateOfLesson) {
+        this.dateOfLesson = dateOfLesson;
+    }
+
+    public int getGradeLevel() {
+        return gradeLevel;
+    }
+
+    public void setGradeLevel(int gradeLevel) {
+        this.gradeLevel = gradeLevel;
+    }
+
+    public ArrayList<Learner> getListOfLearners() {
+        return listOfLearners;
+    }
+
+    public void setListOfLearners(ArrayList<Learner> listOfLearners) {
+        this.listOfLearners = listOfLearners;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTimeSlot() {
+        return timeSlot;
+    }
+
+    public void setTimeSlot(String timeSlot) {
+        this.timeSlot = timeSlot;
+    }
+}
