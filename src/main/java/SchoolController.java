b84b17786dc68a9111ff5302b39efd4a78984060
Tarek <tarekomran_98@hotmail.com>
2024-03-13 14:42:31 +0000

    Handled the grade conditions

--- src/main/java/SchoolController.java
+++ src/main/java/SchoolController.java
@@ -3,7 +3,15 @@ public class SchoolController {
     public LearnerController learnerController = new LearnerController();
     public LessonController lessonController = new LessonController();
 
-    public void linkLearnerToLesson(Learner inputLearner, Lesson inputLesson){
+    public void bookLearnerToLesson(Learner inputLearner, Lesson inputLesson) throws Exception {
+        if(inputLearner == null || inputLesson == null){
+            return;
+        }
+
+        if(inputLearner.getGradeLevel() != inputLesson.getGradeLevel() && (inputLearner.getGradeLevel() + 1) != inputLesson.getGradeLevel()){
+            throw new Exception("The learner is ineligible to book the lesson due to the grade level restriction. Lesson's grade is " + inputLesson.getGradeLevel() + " while the learner is a grade " + inputLearner.getGradeLevel() + " swimmer.");
+        }
+
         learnerController.addNewLearner(inputLearner);
         lessonController.addNewLesson(inputLesson);
         lessonController.getLessonByID(inputLesson.getId()).bookLesson(inputLearner.getId());
